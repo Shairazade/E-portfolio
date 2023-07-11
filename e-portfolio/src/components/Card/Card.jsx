3654858d31609eef1ae4729943e72a3006537aa7
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-07-11 15:15:50 +0200

    changement des images
    suppression des photos google image qui ne s'affichais pas et remplacé par postimage

--- e-portfolio/src/components/Card/Card.jsx
+++ e-portfolio/src/components/Card/Card.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Card({cover, title, description, language}) {
   return (
     <article className="card-projet">
-    <img src={cover} alt="projet" />
-    <div className="card-projet_description">
-        <p className="card-projet_title">{title}</p>
+    <div className='card-image'><img src={cover} alt="projet"/></div>
+    <div className="card-projet_body">
+        <h2 className="card-projet_title">{title}</h2>
         <p className="card-projet_description">{description}</p>
-        <div className='card-projet_language'>{language}</div>
+        <div className='card-projet_language'><img src= {language} alt="langage de programmation"/></div>
     </div>
 </article>
   );
