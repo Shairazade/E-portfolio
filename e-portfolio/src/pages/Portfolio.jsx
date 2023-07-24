0c8b0c482bbc3c7242a722e36a0296800839c38b
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-07-24 10:43:56 +0200

    update a propos

--- e-portfolio/src/pages/Portfolio.jsx
+++ e-portfolio/src/pages/Portfolio.jsx
@@ -16,7 +16,7 @@ export default function Portfolio() {
     <div className='portfolio-container'> {/*voir style dans card.scss */}
         <div className='portfolio-description'> {/*voir style dans card.scss */}
           <h2 className='h1Size'>A propos de moi</h2>
-          <p>Je suis Shairazade Hassani, développeuse intégratrice web, mon objectif est de donner vie à vos projets. Je suis à l'écoute de vos besoins, force de proposition et en quête de nouveaux défis. Prête à m'investir pleinement dans votre entreprise, je m'adapte avec agilité pour offrir des solutions sur mesure. Chaque projet démontre mon savoir-faire, associant créativité et compétences techniques pour concrétiser vos idées les plus ambitieuses. Parcourez mon portfolio pour découvrir l’ensemble des projets que j’ai réalisés. Si vous voulez en savoir plus sur mon parcours, n’hésitez pas :</p>
+          <p>Je suis Shairazade Hassani, développeuse intégratrice web, mon objectif est de donner vie à vos projets. Je suis à l'écoute de vos besoins, force de proposition et en quête de nouveaux défis. Prête à m'investir pleinement dans votre projet, je m'adapte avec agilité pour offrir des solutions sur mesure. Chaque projet démontre mon savoir-faire, associant créativité et compétences techniques pour concrétiser vos idées les plus ambitieuses. Parcourez mon portfolio pour découvrir l’ensemble des projets que j’ai réalisés. Si vous voulez en savoir plus sur mon parcours, n’hésitez pas :</p>
           <div className="btn-center">
           <Button to= "https://github.com/Shairazade?tab=repositories" text="Voir mon CV"/>
           </div>
